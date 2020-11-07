1d99c1a22bfbad258a0f0daf6d8e899fa7ddbb66
Dave Austin <d.austin_391@yahoo.com>
2020-11-07 12:41:06 -0500

    added course model manager

--- course/models.py
+++ course/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Validator(models.Manager):
+    def validate(self, form_data):
+        errors = {}
+        for key, value in form_data.items():
+            if key == "name" and len(value) <= 5:
+                errors[key] = "Course name must be more than 5 characters."
+            elif key == "desc" and len(value) <= 15:
+                errors[key] = "Description must be more than 15 characters."
+        return errors
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=64)
+    desc = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = Validator()
--- course/views.py
+++ course/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Course
 
 
 def index(request):
-    return render(request, "index.html")
+    context = {"all_courses": Course.objects.all()}
+    return render(request, "index.html", context)
 
 
 def create(request):
     if request.method == "POST":
-        pass
-    else:
-        return redirect("/")
+        errors = Course.objects.validate(request.POST)
+        if not errors:
+            Course.objects.create(name=request.POST["name"], desc=request.POST["desc"])
+        else:
+            for msg in errors.values():
+                messages.error(request, msg)
+    return redirect("/")
